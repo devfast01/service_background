a91b507d2d08b1191a23bd090cfb018a543c8fee
devfast01 <iot123tech@gmail.com>
2023-12-12 14:59:22 +0500

    Error is Example_2

---
+++ app/src/main/java/com/example/background_service/Example_3/Example3_activity.kt
@@ -0,0 +1,12 @@
+package com.example.background_service.Example_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.background_service.R
+
+class Example3_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example3)
+    }
+}
